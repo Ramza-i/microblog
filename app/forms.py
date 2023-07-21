bb1d0cc5d2a35b45cb2ab748a144ecfe2417e5b7
Ramzai <Ramzai@a.tv>
2023-07-21 13:02:29 +0700

    new: PostForm

--- app/forms.py
+++ app/forms.py
@@ -52,3 +52,7 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=username.data).first()
             if user is not None:
                 raise ValidationError('Username занят')
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Скажите что-то', validators=[DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Отправить')
