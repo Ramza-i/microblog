9c51487fe05ef66aec44280ea831df1844f5e70e
Ramzai <porfirev.o@gmail.com>
2023-07-22 19:27:01 +0700

    new:routes: add 400,500 errorhandler, templates for this errors

--- app/routes.py
+++ app/routes.py
@@ -139,3 +139,13 @@ def unfollow(username):
     db.session.commit()
     flash(f'Вы отписались от {username}.')
     return redirect(url_for('user', username=username))
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
