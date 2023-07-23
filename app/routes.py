5dd0a103f436d9746f18cb9f543cf9b85fff30f5
Ramzai <porfirev.o@gmail.com>
2023-07-23 13:32:00 +0700

    new:routes: moving 400,500 errorhandler to another file

--- app/routes.py
+++ app/routes.py
@@ -140,12 +140,4 @@ def unfollow(username):
     flash(f'Вы отписались от {username}.')
     return redirect(url_for('user', username=username))
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_error(error):
-    db.session.rollback()
-    return render_template('500.html'), 500
 
