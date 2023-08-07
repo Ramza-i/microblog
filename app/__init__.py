a690c6c17b9916192775bd92decac434e3e892e3
Ramzai <porfirev.o@gmail.com>
2023-08-07 22:15:01 +0700

    mod: blueprint: main

--- app/__init__.py
+++ app/__init__.py
@@ -33,6 +33,9 @@ app.register_blueprint(errors_bp)                   #Регистрируем er
 from app.auth import bp as auth_bp
 app.register_blueprint(auth_bp, url_prefix='/auth')
 
+from app.main import bp as main_bp
+app.register_blueprint(main_bp)
+
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
@@ -60,4 +63,5 @@ if not app.debug:
     app.logger.info('Microblog startup')
 
 
-from app import routes, models
+from app import models
+# from app.main import routes
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,51 +1,51 @@
-from app import app, db
-from flask import render_template, flash, redirect, url_for, request
-from app.forms import EditProfileForm, PostForm, EmptyForm
-from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User, Post
-from werkzeug.urls import url_parse
-from werkzeug.utils import secure_filename
 import os.path
 from datetime import datetime
-from app.email import send_password_reset_email
+from app import app, db
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_required
+from werkzeug.utils import secure_filename
 
+from app.main.forms import EditProfileForm, PostForm, EmptyForm
+from app.models import User, Post
 
-@app.before_request
+from app.main import bp
+
+@bp.before_request
 def before():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     form = PostForm()
     page = request.args.get('page', 1, type=int)           # Получаем из GET-запроса значение по ключу page, если его в запросе нет - 1. Параметры в запрос можем пихать сами через &
     posts = current_user.followed_posts().paginate(page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    next_url = url_for('main.index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('main.index', page=posts.prev_num) if posts.has_prev else None
     if form.validate_on_submit():
         post = Post(body=form.post.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Пост отправлен!')
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
 
     return render_template('index.html', title='Главная', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
-@app.route('/explore')
+@bp.route('/explore')
 @login_required
 def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+    next_url = url_for('main.explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('main.explore', page=posts.prev_num) if posts.has_prev else None
     return render_template('index.html', title='Все посты', posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
 
-@app.route('/user/<username>', methods=['GET', 'POST'])
+@bp.route('/user/<username>', methods=['GET', 'POST'])
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
@@ -53,12 +53,12 @@ def user(username):
     userlist = User.query.all()
     page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
+    next_url = url_for('main.user', username=user.username, page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('main.user', username=user.username, page=posts.prev_num) if posts.has_prev else None
     return render_template('user.html', user=user, title=f'Home|{username}', userlist=userlist, posts=posts.items, next_url=next_url, prev_url=prev_url, form=form)
 
 
-@app.route('/edit_profile', methods=['GET', 'POST'])
+@bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
     form = EditProfileForm(current_user.username)
@@ -72,14 +72,14 @@ def edit_profile():
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Изменения сохранены')
-        return redirect(url_for('user', username=current_user.username))
+        return redirect(url_for('main.user', username=current_user.username))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', form=form, title=f'{current_user.username}|Редактирование')
 
 
-@app.route('/follow/<username>', methods=['POST'])
+@bp.route('/follow/<username>', methods=['POST'])
 @login_required
 def follow(username):
     form = EmptyForm()
@@ -87,18 +87,18 @@ def follow(username):
         user = User.query.filter_by(username=username).first()
         if user is None:
             flash(f'User {username} not found.')
-            return redirect(url_for('index'))
+            return redirect(url_for('main.index'))
         if user == current_user:
             flash('You cannot follow yourself!')
-            return redirect(url_for('user', username=username))
+            return redirect(url_for('main.user', username=username))
         current_user.follow(user)
         db.session.commit()
         flash(f'Вы подписаны на {username}!')
-        return redirect(url_for('user', username=username))
+        return redirect(url_for('main.user', username=username))
     else:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
 
-@app.route('/unfollow/<username>', methods=['POST'])
+@bp.route('/unfollow/<username>', methods=['POST'])
 @login_required
 def unfollow(username):
     form = EmptyForm()
@@ -106,15 +106,15 @@ def unfollow(username):
         user = User.query.filter_by(username=username).first()
         if user is None:
             flash(f'User {username} not found.')
-            return redirect(url_for('index'))
+            return redirect(url_for('main.index'))
         if user == current_user:
             flash('You cannot unfollow yourself!')
-            return redirect(url_for('user', username=username))
+            return redirect(url_for('main.user', username=username))
         current_user.unfollow(user)
         db.session.commit()
         flash(f'Вы отписались от {username}.')
-        return redirect(url_for('user', username=username))
+        return redirect(url_for('main.user', username=username))
     else:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
 
 
